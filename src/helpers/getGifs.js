f6612c649d628b3623ba1df3cae02800cfdf67b1
Julian Gonzalez <juliangon25@gmail.com>
2020-07-01 17:18:21 -0300

    first commit-

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=gocfmoVM3lfYtn2Ffg4OQyzFg8RaC9lF`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url,
+        }
+    });
+    
+    console.log(gifs);
+    return gifs;
+}

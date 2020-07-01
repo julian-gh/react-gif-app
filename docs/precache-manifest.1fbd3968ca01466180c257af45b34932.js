09beca75c88ebc15be089549880775b5703f41b9
Julian Gonzalez <juliangon25@gmail.com>
2020-07-01 17:19:45 -0300

    prod build added

---
+++ docs/precache-manifest.1fbd3968ca01466180c257af45b34932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c630689a4edb09fa91937f175ef4974b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b696ad89618d08ab5708",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "c75c3447e94cde1bffcd",
+    "url": "/static/css/main.8d56265b.chunk.css"
+  },
+  {
+    "revision": "b696ad89618d08ab5708",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75c3447e94cde1bffcd",
+    "url": "/static/js/main.f7c0fb34.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
